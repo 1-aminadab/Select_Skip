79b5278d90f24ee05cf15f2c300f36de0066bd87
1-aminadab <developer.amanuel@gmail.com>
2025-06-09 09:34:44 +0300

    tab logo and title changed

--- src/components/SkipCard.tsx
+++ src/components/SkipCard.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import styles from './styles/SkipCard.module.css';
-import skipImage from '../assets/skip.png';
+import skipImage from '../assets/images/skip.png';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import type { SkipData } from '../types';
